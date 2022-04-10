12309cb07badb9cc29cc4213ca923794f0a72bb5
jasonsie88 <56729568+jasonsie88@users.noreply.github.com>
2022-04-11 00:24:39 +0800

    Delete hw5-jasonsie88-master directory

--- hw5-jasonsie88-master/src/include/sema/error.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SEMA_ERROR_H
-#define SEMA_ERROR_H
-
-struct Location;
-
-void logSemanticError(const Location &, const char *format, ...);
-
-#endif
--- hw5-jasonsie88-master/src/lib/AST/assignment.cpp
+++
@@ -1,6 +0,0 @@
-#include "AST/assignment.hpp"
-
-void AssignmentNode::visitChildNodes(AstNodeVisitor &p_visitor) {
-    m_lvalue->accept(p_visitor);
-    m_expr->accept(p_visitor);
-}
